6dd8a4ee2c5f7c54426cfb05d98594821fcd64ed
Sharath Sarangmath <40777592+sharathsarangmath@users.noreply.github.com>
2026-08-25 16:33:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'steady-v5';
+const CACHE = 'steady-v6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
